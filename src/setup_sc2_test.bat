d2e949cbeecd0f383a58e67ccadcf5e436cc0d1b
Kobe <koberunnels@gmail.com>
2023-04-12 17:10:38 -0500

    added checks for compiling and full main loop into testing

--- src/setup_sc2_test.bat
+++ src/setup_sc2_test.bat
@@ -1,3 +1,8 @@
 echo Testing build...
 gcc test.c -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -o "SC2_test"
-SC2_test
+
+if %errorlevel% == 0 (
+	SC2_test
+) else  (
+	echo "Test could not compile";
+)
